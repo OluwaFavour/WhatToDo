c67eda5aca11cf1b6fb22c76dd60019a232bdc11
Favour Olusayo <fsticks8187@gmail.com>
2021-09-30 04:41:35 +0100

    Add project files.

---
+++ Migrations/20210930033902_addTasksToDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhatToDo.Migrations
+{
+    public partial class addTasksToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Color = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ Models/_Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatToDo.Models
+{
+    public enum Color
+    {
+        RED,
+        GREEN,
+        YELLOW,
+        WHITE,
+    }
+    
+    public class _Task
+    // Task Model for the to-do list
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Action { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Color Color { get; set; }
+
+        public _Task()
+        {
+            this.DateCreated = DateTime.Now;
+        }
+    }
+}
